5055f96801be2b003ba9a9ad41dc9d83375fa2f1
Nuno Martins <nmartins@redhat.com>
2025-09-11 16:22:22 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -54,11 +54,6 @@ tee /tmp/setup.yml << EOF
   #become: true
   tasks:
 
-    # - name: Add search to resolv.conf
-    #   ansible.builtin.shell:
-    #     cmd: echo "search $_SANDBOX_ID.svc.cluster.local." >> /etc/resolv.conf
-    #   become: true
-
     - name: Install EPEL
       ansible.builtin.package:
         name: https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
@@ -85,51 +80,6 @@ tee /tmp/setup.yml << EOF
         - podman-compose
         - python3-dotenv
       become: true
-
-    # - name: Clone gitea podman-compose project
-    #   ansible.builtin.git:
-    #     repo: https://github.com/cloin/gitea-podman.git
-    #     dest: /tmp/gitea-podman
-    #     force: true
-
-    # - name: Allow user to linger
-    #   ansible.builtin.command: 
-    #     cmd: loginctl enable-linger rhel
-    #     chdir: /tmp/gitea-podman
-
-    # - name: Start gitea
-    #   ansible.builtin.command: 
-    #     cmd: podman-compose up -d
-    #     chdir: /tmp/gitea-podman
-
-    # - name: Wait for gitea to start
-    #   ansible.builtin.pause:
-    #     seconds: 15
-
-    # - name: Create gitea student user
-    #   ansible.builtin.shell:
-    #     cmd: podman exec -u git gitea /usr/local/bin/gitea admin user create --admin --username student --password learn_ansible --email student@example.com
-
-    # - name: Create gitea ansible user
-    #   ansible.builtin.shell:
-    #     cmd: podman exec -u git gitea /usr/local/bin/gitea admin user create --admin --username ansible --password learn_ansible --email ansible@example.com
-
-    # - name: Migrate github projects to gitea student user
-    #   ansible.builtin.uri:
-    #     url: http://podman:3000/api/v1/repos/migrate
-    #     method: POST
-    #     body_format: json
-    #     body: {"clone_addr": "{{ item.url }}", "repo_name": "{{ item.name }}"}
-    #     status_code: [201, 409]
-    #     headers:
-    #       Content-Type: "application/json"
-    #     user: student
-    #     password: learn_ansible
-    #     force_basic_auth: yes
-    #     validate_certs: no
-    #   loop:
-    #     - {name: 'eda-project', url: 'https://github.com/cloin/eda-project-basic.git'}
-    #     - {name: 'eda-alertmanager', url: 'https://github.com/cloin/eda-alertmanager.git'}
 
     - name: Set the default branch to aap25 for migrated repositories
       ansible.builtin.uri:
@@ -405,10 +355,6 @@ tee /tmp/setup.yml << EOF
   become: true
   tasks:
 
-    - name: Add search to resolv.conf
-      ansible.builtin.shell:
-        cmd: echo "search $_SANDBOX_ID.svc.cluster.local." >> /etc/resolv.conf
-      become: true
 
     - name: Install epel-release
       ansible.builtin.dnf:
