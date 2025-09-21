2169213c1afed90f333b39e26ee862117fbfd5c4
Nuno Martins <nmartins@redhat.com>
2025-09-21 14:11:27 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+nmcli connection add type ethernet con-name enp2s0 ifname enp2s0 ipv4.addresses 192.168.1.10/24 ipv4.method manual connection.autoconnect yes
+nmcli connection up enp2s0
+echo "192.168.1.10 control.lab control" >> /etc/hosts
+echo "192.168.1.11 podman.lab podman" >> /etc/hosts
+echo "192.168.1.12 rhel-1.lab podman" >> /etc/hosts
+echo "192.168.1.13 rhel-2.lab podman" >> /etc/hosts
+
+
 systemctl stop systemd-tmpfiles-setup.service
 systemctl disable systemd-tmpfiles-setup.service
 
